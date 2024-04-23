3519357b39b8aaffcd64c52ce7cad30c16ba8375
Sigit Kurniawan <skurniawan24@bsd.idserve.net>
2024-04-24 06:17:09 +0700

    Initial Commit

---
+++ dBMeter/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  dBMeter
+//
+//  Created by Sigit Academy on 24/04/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ dBMeter/dBMeterApp.swift
@@ -0,0 +1,17 @@
+//
+//  dBMeterApp.swift
+//  dBMeter
+//
+//  Created by Sigit Academy on 24/04/24.
+//
+
+import SwiftUI
+
+@main
+struct dBMeterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
